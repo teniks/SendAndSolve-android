71b5f46fcf449f96b4e5c11806f2097c79d77a1a
teniks <teniks69@yandex.ru>
2025-05-01 21:51:33 +0300

    feat(DetailTaskFragment): add auxiliary classes for task priority and status

---
+++ app/src/main/java/su/sendandsolve/features/tasks/domain/enums/TaskPriority.kt
@@ -0,0 +1,13 @@
+package su.sendandsolve.features.tasks.domain.enums
+
+/**
+ * CRITICAL - important and urgent.
+ * STRATEGIC - important, but not urgent.
+ * TACTICAL - urgent, but not important.
+ * BACKGROUND - neither important, nor urgent.
+ * @author: teniks
+ * @since: 01.05.2025
+ */
+enum class TaskPriority{
+    CRITICAL, STRATEGIC, TACTICAL, BACKGROUND;
+}
---
+++ app/src/main/java/su/sendandsolve/features/tasks/domain/enums/TaskStatus.kt
@@ -0,0 +1,9 @@
+package su.sendandsolve.features.tasks.domain.enums
+
+enum class TaskStatus {
+    NEW,
+    IN_PROGRESS,
+    ON_HOLD,
+    COMPLETED,
+    CANCELLED;
+}
