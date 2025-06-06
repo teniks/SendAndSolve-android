8da414d2ae6b47449fbe894ceb23e7a770a27a79
teniks <teniks69@yandex.ru>
2025-06-06 13:40:50 +0300

    feat(PasswordHasher): update the password encryption algorithm.
    - Replace SHA-256 to PBKDF2.

--- app/src/main/java/su/sendandsolve/features/auth/utils/PasswordHasher.kt
+++ app/src/main/java/su/sendandsolve/features/auth/utils/PasswordHasher.kt
@@ -1,24 +1,50 @@
 package su.sendandsolve.features.auth.utils
 
 import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import java.security.SecureRandom
 import java.util.Base64
+import javax.crypto.SecretKeyFactory
+import javax.crypto.spec.PBEKeySpec
 
 object PasswordHasher {
 
+    private const val ITERATIONS = 65536
+    private const val HASH_LENGTH = 256
+
     fun generateSalt(): String {
-        val random = ByteArray(32)
-        java.security.SecureRandom().nextBytes(random)
-        return Base64.getEncoder().encodeToString(random)
+        val salt = generateSaltBytes()
+        return Base64.getEncoder().encodeToString(salt)
+    }
+
+    private fun generateSaltBytes(): ByteArray {
+        val random = SecureRandom()
+        val salt = ByteArray(64)
+        random.nextBytes(salt)
+        return salt
     }
 
     fun hash(password: String, salt: String): String {
-        val digest = MessageDigest.getInstance("SHA-256")
-        val saltedPassword = salt + password
-        val hashBytes = digest.digest(saltedPassword.toByteArray())
-        return Base64.getEncoder().encodeToString(hashBytes)
+        val saltBytes = Base64.getDecoder().decode(salt)
+        val hash = hash(password, saltBytes)
+        return Base64.getEncoder().encodeToString(hash)
+    }
+
+    private fun hash(password: String, salt: ByteArray): ByteArray {
+        val spec = PBEKeySpec(password.toCharArray(), salt, ITERATIONS, HASH_LENGTH)
+
+        try {
+            val factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512")
+            return factory.generateSecret(spec).encoded
+        } catch (e: NoSuchAlgorithmException) {
+            throw RuntimeException(e)
+        }
     }
 
     fun verify(password: String, salt: String, storeHash: String): Boolean {
-        return hash(password, salt) == storeHash
+        val saltBytes = Base64.getDecoder().decode(salt)
+        val storedHashBytes = Base64.getDecoder().decode(storeHash)
+        val newHash = hash(password, saltBytes)
+        return MessageDigest.isEqual(newHash, storedHashBytes)
     }
 }
